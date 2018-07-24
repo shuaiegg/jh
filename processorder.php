e14e1c893ccff03c52988c5180ec299bc24933c4
jack <sendymusic@hotmail.com>
2018-07-24 17:03:03 +0800

    first php file

--- processorder.php
+++ processorder.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Jack's project</title>
-</head>
+<title>Jack's order</title>
 <body>
+<h1>jack's order</h1>
+<h2>Order Results</h2>
+<?php 
+ echo "<p>Order processed at";
+ echo date('H:i, jS F Y');
+ echo "</p>"
 
-<h1>Jack's auto parts</h1>
-<h2>order results</h2>
+?>
+
+<?php
+ // create short variable names
+$guitarqty = $_POST['guitarqty'];
+$ampqty = $_POST['ampqty'];
+$stringqty = $_POST['stringqty'];
+?>
+
+<?php
+
+echo '<p>Your order is as follows:</p>';
+
+echo htmlspecialchars($guitarqty).' guitars<br />';
+
+echo htmlspecialchars($ampqty).' amps<br />';
+
+echo htmlspecialchars($stringqty).' strings<br />';
+
+?> 
+
+
+
+
+
 
 
 </body>
